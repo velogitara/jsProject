d908cba86a3e454b8fb09879fe62eb89fd624061
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-01-18 21:56:25 +0200

    1

---
+++ JS/script.js
@@ -0,0 +1,88 @@
+"use strict";
+
+// let myLifeStile = 'freelance';
+// let myName = 'alex',
+//     myAge = '30',
+//     myMessage = 'ура';
+// let myNewAge = '13';
+
+// myAge = '15';
+
+// myAge = myNewAge
+
+// console.log('я учу js');
+// console.log('myLifestyle');
+// console.log(myAge);
+
+
+// function testBlock() {
+//     let main = 'privet';
+//     console.log(main);
+// }
+// testBlock()
+
+
+// const userProfile = {
+//     name: 'velogitara',
+//     age: '40',
+//     massage: 'work for life'
+// }
+
+// userProfile.age = 50;
+// console.log(userProfile);
+
+
+// let userColorEye = '';
+// let user = 'Ася';
+// let userName = 'Вася';
+// userName = user
+// console.log(user);
+
+
+
+// let userName = 'null';
+
+// console.log(typeof userName)
+
+// console.log(userName)
+
+
+// let willYouMarryMe = true;
+// if (willYouMarryMe) {
+//     console.log('Ура');
+// } else {
+//     console.log('fuck')
+// }
+
+
+// let trueOrFalse = 58 + 10;
+// console.log(trueOrFalse);
+
+// let userAge = ('30');
+// let userHeight = 1.70;
+// console.log(userAge);
+// console.log(typeof userAge);
+// console.log(userHeight);
+// console.log(typeof userHeight);
+
+
+
+
+let userAge = 100;
+let userAgeInfo = `Возраст: ${userAge}`;
+console.log(userAgeInfo);
+
+// let userAge = 100;
+// console.log(userAge);
+// console.log(typeof userAge);
+
+// userAge = String(userAge);
+// console.log(userAge);
+// console.log(typeof userAge);
+
+
+
+
+
+
+
